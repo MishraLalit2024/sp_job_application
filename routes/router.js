1d8234a5e0826c541a683992e47221e94fc59161
MishraLalit2024 <lalit.mishra2024dev@gmail.com>
2024-04-05 19:37:22 +0530

    Started Validation

--- routes/router.js
+++ routes/router.js
@@ -7,6 +7,10 @@ router.get('/', home);
 
 router.get('/home', homeRenderer);
 
+router.post('/validate', (req, res)=>{
+    res.send("Form submitted successfully.");
+})
+
 router.get('*', home)
 
 
